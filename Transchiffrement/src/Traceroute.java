28e958f1e2b76905aefe1bdb0a3b9a7311ff4477
egenerat <boydoc@hotmail.fr>
2014-01-30 12:58:55 +0100

    modifications jpcap et ajout librairie

--- Transchiffrement/src/Traceroute.java
+++ Transchiffrement/src/Traceroute.java
@@ -19,41 +19,38 @@ public class Traceroute {
 	}
 
 	public Traceroute() throws IOException {
-
 		NetworkInterface[] devices = JpcapCaptor.getDeviceList();
-
-		int i = 0;
-		System.out.println(i + ": " + devices[i].name + "("	+ devices[i].description + ")");
+		NetworkInterface inputInterface = devices[0];
+		NetworkInterface outputInterface = devices[1];
+		System.out.println("0 : " + inputInterface.name + "("	+ inputInterface.description + ")");
 
 		// print out its datalink name and description
-		System.out.println(" datalink: " + devices[i].datalink_name + "(" + devices[i].datalink_description + ")");
+		System.out.println(" datalink: " + inputInterface.datalink_name + "(" + inputInterface.datalink_description + ")");
 
 		// print out its MAC address
 		System.out.print(" MAC address:");
-		for (byte b : devices[i].mac_address)
+		for (byte b : inputInterface.mac_address)
 			System.out.print(Integer.toHexString(b & 0xff) + ":");
 		System.out.println();
 
 		// print out its IP address, subnet mask and broadcast address
-		for (NetworkInterfaceAddress a : devices[i].addresses)
+		for (NetworkInterfaceAddress a : inputInterface.addresses)
 			System.out.println(" address:" + a.address + " " + a.subnet + " " + a.broadcast);
-		int index = 0; // set index of the interface that you want to open.
 
-		// Open an interface with openDevice(NetworkInterface intrface, int
-		// snaplen, boolean promics, int to_ms)
+		// Open an interface with openDevice(NetworkInterface intrface, int snaplen, boolean promics, int to_ms)
 		try {
-			JpcapCaptor captor = JpcapCaptor.openDevice(devices[index], 65535, false, 20);
-			captor.setFilter("ip and tcp", true);
-			captor.loopPacket(10, new PacketPrinter());
+			JpcapCaptor captor = JpcapCaptor.openDevice(inputInterface, 65535, false, 20);
+			captor.setFilter("tcp", true);
+			captor.loopPacket(100, new PacketPrinter(outputInterface));
 			captor.close();
 		} catch (Exception e) {
 			System.out.println("Exception");
 		}
 		
 		//open a network interface to send a packet to
-		JpcapSender sender=JpcapSender.openDevice(devices[index]);
+		//JpcapSender sender=JpcapSender.openDevice(outputInterface);
 
-		//create a TCP packet with specified port numbers, flags, and other parameters
+		/*//create a TCP packet with specified port numbers, flags, and other parameters
 		TCPPacket p=new TCPPacket(12,34,56,78,false,false,false,false,true,true,true,true,10,10);
 
 		//specify IPv4 header parameters
@@ -74,20 +71,11 @@ public class Traceroute {
 		//set the datalink frame of the packet p as ether
 		p.datalink=ether;
 
-		//send the packet p
-		sender.sendPacket(p);
-		System.out.println("Sent !");
-		sender.close();
-		
-		/*TCPPacket p = (TCPPacket)packet;
-
-		// Get the tcp src and dest ports
-		int destPort = p.dst_port;
-		int srcPort = p.src_port;
-
-		// Get the src and dest IP addresses from the IP layer
-		InetAddress destIp = p.dst_ip;
-		InetAddress srcIp = p.src_ip;*/
+		//send the packet p*/
+		//sender.sendPacket(p);
+		//System.out.println("Sent !");
+		//sender.close();
 		// }
 	}
 }
+
