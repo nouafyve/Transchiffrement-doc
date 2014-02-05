00e3f95864c261c451bbd83f6ae7b29f6fbe7c30
egenerat <boydoc@hotmail.fr>
2014-02-05 17:13:44 +0100

    proxy http en cours

--- Transchiffrement/src/ProxySSI.java
+++ Transchiffrement/src/ProxySSI.java
@@ -7,6 +7,7 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.KeyStore;
+import java.security.cert.Certificate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,8 +15,6 @@ import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManagerFactory;
-import javax.security.cert.X509Certificate;
 
 public class ProxySSI {
 	public static void main(String[] args) throws Exception {
@@ -26,7 +25,6 @@ public class ProxySSI {
 		byte[] buf = new byte[40960];
 		byte[] buffer2 = new byte[40960];
 		int count = -1;
-		int count2 = -1;
 		String line;
 
 		BufferedInputStream in = new BufferedInputStream(connectionSocket.getInputStream(), buf.length);
@@ -37,15 +35,14 @@ public class ProxySSI {
 			System.out.println(line);
 
 			// TODO regex ???
-			Pattern m_httpsConnectPattern = Pattern.compile(
-					"^CONNECT[ \\t]+([^:]+):(\\d+).*\r\n\r\n", Pattern.DOTALL);
+			Pattern m_httpsConnectPattern = Pattern.compile("^CONNECT[ \\t]+([^:]+):(\\d+).*\r\n\r\n", Pattern.DOTALL);
 			Matcher httpsConnectMatcher = m_httpsConnectPattern.matcher(line);
 			
-			if (httpsConnectMatcher.find()) {
-
-				//TODO Flush ?
-				//while (in.read(buf, 0, in.available()) > 0) {}
-				
+//			Pattern m_httpGETPattern = Pattern.compile("^GET[ \\t]+([^:]+):(\\d+).*\r\n\r\n", Pattern.DOTALL);
+//			Matcher httpGETMatcher = m_httpGETPattern.matcher(line);
+			
+			
+			if (httpsConnectMatcher.find()) {				
 				String remoteHost = httpsConnectMatcher.group(1);
 				int remotePort = Integer.parseInt(httpsConnectMatcher.group(2));
 				System.out.println("HTTPS SSL/TLS : "+httpsConnectMatcher.group(1)+" "+httpsConnectMatcher.group(2));
@@ -60,114 +57,98 @@ public class ProxySSI {
 				System.out.println("réponse 200 ok");
 								
 				
-				/***********************************/
-				
+				/***********************************/	
 
 		        /* Load properties */
 		        
 				System.setProperty("https.protocols","TLSv1");
 				String keyStoreFile = "/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/security/cacerts";
 				char[] keyStorePassword = "changeit".toCharArray();
-		        
-		        /* Create keystore */
-		        KeyStore keyStore = null; // = KeyStore.getInstance(KeyStore.getDefaultType());
 
 	        	// Que fait le 2ème paramètre ?
 	        	String keyStoreType = "jks";
-	        	keyStore = KeyStore.getInstance(keyStoreType);
+	        	KeyStore keyStore = KeyStore.getInstance(keyStoreType);
 	        	keyStore.load(new FileInputStream(keyStoreFile), keyStorePassword);
-	        	System.out.println("On a ouvert un keystoreFile " +keyStore);
 
-		        /* Get factory for the given keystore */
-		        //TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-		        //tmf.init(keyStore);		        
+		        /* Get factory for the given keystore */	        
 		        SSLContext ctx = SSLContext.getInstance("TLS");
 		        KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
 		        
-		        KeyStore.Entry entry = keyStore.getEntry("mykey", new KeyStore.PasswordProtection("000000".toCharArray()));
-		        
+		        //KeyStore.Entry entry = keyStore.getEntry("mykey", new KeyStore.PasswordProtection("000000".toCharArray()));
+
 		        kmf.init(keyStore, "000000".toCharArray());
 		        ctx.init(kmf.getKeyManagers(), null, null);
-		        
 		        SSLSocketFactory factory = ctx.getSocketFactory();
-
-		        
-		        
-		        
-		        
-		        
-		        
-		        
-		        
-		        
-		        
-		        
-		        /*
-		    	PrivateKey pk = (PrivateKey) keyStore.getKey(JSSEConstants.DEFAULT_ALIAS,keyStorePassword);
-		    	iaik.x509.X509Certificate newCert = SignCert.forgeCert(keyStore, keyStorePassword,JSSEConstants.DEFAULT_ALIAS,  remoteCN, remoteServerCert);
-		    	KeyStore newKS = KeyStore.getInstance("jks");
-		    	newKS.load(null, null);
-		    	newKS.setKeyEntry(JSSEConstants.DEFAULT_ALIAS, pk, keyStorePassword, new Certificate[] {newCert});
-		    	keyManagerFactory.init(newKS, keyStorePassword);
-		    	m_sslContext.init(keyManagerFactory.getKeyManagers(),  new TrustManager[] { new TrustEveryone() }, null);
-		    	m_serverSocketFactory = m_sslContext.getServerSocketFactory(); */
-		        
-		        
-		        
-		        
-		        
-		        
 		        
 		        
 		        /*********************************************/
 				
 				try {
-					// Client => Proxy					
-					SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-					// TODO
-					String clientHost = connectionSocket.getInetAddress().toString();
-					System.out.println("Test : "+clientHost);
-					
-					SSLSocket sslSocket = (SSLSocket) factory.createSocket(connectionSocket, "127.0.0.1", connectionSocket.getPort(), false);
-					sslSocket.setEnabledCipherSuites(sslSocket.getSupportedCipherSuites());
 									
-					
-					for( String i : sslSocket.getEnabledProtocols()){
-						System.out.println("Julien : "+i);
-					}
-					
-					sslSocket.setUseClientMode(false);
+					SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
 					
 					// Proxy <=> Server
 					SSLSocket sslsocketClient = (SSLSocket) sslsocketfactory.createSocket(remoteHost, remotePort);
-					
 					sslsocketClient.setEnabledCipherSuites(sslsocketClient.getSupportedCipherSuites());
-				
-					
-					System.out.println(sslsocketClient.getSession().getPeerCertificates()[0]);
-					
+					//Certificate serveurCert = sslsocketClient.getSession().getPeerCertificates()[0]; 
 					OutputStream requeteAuServeurWeb = sslsocketClient.getOutputStream();
 					InputStream reponseDuServeurWeb = sslsocketClient.getInputStream();
 					
 					requeteAuServeurWeb.write("GET / \n".getBytes());
+					reponseDuServeurWeb.read(buffer2);
+					System.out.println(new String(buffer2));
 					
 					//ETAPE 4
 					//SSLSOCKET = Client <-> Proxy
+					// Client => Proxy
+					// Pour récupérer l'adresse du client
+					//String clientHost = connectionSocket.getInetAddress().toString();					
+					
+			        //keyStore.setKeyEntry("monserv", "ch	ngeit".getBytes(), new Certificate[]{serveurCert});
+					
+			        /*
+			    	PrivateKey pk = (PrivateKey) keyStore.getKey(JSSEConstants.DEFAULT_ALIAS,keyStorePassword);
+			    	iaik.x509.X509Certificate newCert = SignCert.forgeCert(keyStore, keyStorePassword,JSSEConstants.DEFAULT_ALIAS,  remoteCN, remoteServerCert);
+			    	KeyStore newKS = KeyStore.getInstance("jks");
+			    	newKS.load(null, null);
+			    	newKS.setKeyEntry(JSSEConstants.DEFAULT_ALIAS, pk, keyStorePassword, new Certificate[] {newCert});
+			    	keyManagerFactory.init(newKS, keyStorePassword);
+			    	m_sslContext.init(keyManagerFactory.getKeyManagers(),  new TrustManager[] { new TrustEveryone() }, null);
+			    	m_serverSocketFactory = m_sslContext.getServerSocketFactory(); */
+					
+					SSLSocket sslSocket = (SSLSocket) factory.createSocket(connectionSocket, "127.0.0.1", connectionSocket.getPort(), false);
+					sslSocket.setEnabledCipherSuites(sslSocket.getSupportedCipherSuites());
+					sslSocket.setUseClientMode(false);
 					OutputStream outputStreamClient = sslSocket.getOutputStream();
 					
-					while ((count2 = reponseDuServeurWeb.read(buffer2)) > 0) {
-						System.out.println(new String(buffer2)); 
-						outputStreamClient.write(buffer2);
-						System.out.println("passe pas");
-					}
-					//outputStreamClient.flush();
-					System.out.println("Bim le client.... ou pas :/");
-				} catch (IOException e) {
+					// Réponse qu'on envoie au client
+					outputStreamClient.write(buffer2);
+					outputStreamClient.flush();
+				}
+				catch (IOException e) {
 					e.printStackTrace();
 				}
+			}
+			//else if (httpGETMatcher.find()){
+			else{
+				System.out.println("Entrée en mode HTTP");
+				Socket clientSocket = new Socket("192.168.56.101", 80);
+			
+				BufferedOutputStream requeteServeur = new BufferedOutputStream(clientSocket.getOutputStream(), buffer2.length);
+				BufferedInputStream reponseServeur = new BufferedInputStream(clientSocket.getInputStream(), buffer2.length);
+				System.out.println("1111");
+				//requeteServeur.write("GET / HTTP/1.1 \n".getBytes());
+				requeteServeur.write(buf);
+				System.out.println("222");
+				reponseServeur.read(buffer2);
+				System.out.println(new String(buffer2));
+				System.out.println("333");
+				out.write(buffer2);
+				System.out.println("Sortie HTTP");
+				clientSocket.close();
 			}
 		}
 		connectionSocket.close();
 		serverSocket.close();
-	}
+	} 
 }
