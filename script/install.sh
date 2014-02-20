3014f8ab7e5c248048467b5621a01bace2b86cac
egenerat <boydoc@hotmail.fr>
2014-02-20 10:39:27 +0100

    script ok même sans la clé acceptée auparavant

--- script/install.sh
+++ script/install.sh
@@ -6,7 +6,7 @@ else
 	victime=$1
 	chemin=~/Transchiffrement-doc/script/
 	password=$2
-	sshpass -p "$password" scp $chemin/* $victime:~/ 
+	sshpass -p "$password" scp -oStrictHostKeyChecking=no $chemin/* $victime:~/ 
 	echo "COPIE FINIE"
 	sshpass -p "$password" ssh $victime -o RequestTTY=yes "chmod 755 del.sh; ./del.sh $3" 
 fi
